bd5b42ce8e397d31c838e0b672ba0cedbf85d0f9
Abhishek Khandelwal <abhishekkhandelwal47@gmail.com>
2023-06-04 17:48:42 +0530

    cors fix

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -5,34 +5,38 @@ export const useLogin = () => {
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(null);
     const { dispatch } = useAuthContext();
+    try {
+        const login = async (email, password) => {
+            setIsLoading(true);
+            setError(null);
 
-    const login = async (email, password) => {
-        setIsLoading(true);
-        setError(null);
+            const response = await fetch('https://lazy-tan-worm-garb.cyclic.app/api/user/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ email, password })
+            })
 
-        const response = await fetch('/api/user/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ email, password })
-        })
+            const json = await response.json();
 
-        const json = await response.json();
+            if (!response.ok) {
+                setIsLoading(false);
+                setError(json.error);
+            }
 
-        if (!response.ok) {
-            setIsLoading(false);
-            setError(json.error);
+            if (response.ok) {
+                // save the user to local storage
+                localStorage.setItem('user', JSON.stringify(json));
+
+                // update the auth context
+                dispatch({ type: 'LOGIN', payload: json });
+
+                setIsLoading(false);
+            }
         }
 
-        if (response.ok) {
-            // save the user to local storage
-            localStorage.setItem('user', JSON.stringify(json));
-
-            // update the auth context
-            dispatch({ type: 'LOGIN', payload: json });
-
-            setIsLoading(false);
-        }
+        return { login, isLoading, error }
+    } catch (e) {
+        return e
     }
 
-    return { login, isLoading, error }
 }
